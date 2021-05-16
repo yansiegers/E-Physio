3f0419e711d7b1e1d6f3e812badb9457b8b16993
Yan Siegers <email@yansiegers.nl>
2021-05-16 15:44:02 +0200

    Install Tailwind

---
+++ resources/views/includes/styles.blade.php
@@ -0,0 +1,6 @@
+{{--<link rel="preconnect" href="https://fonts.gstatic.com">--}}
+{{--<link href="https://fonts.googleapis.com/css2?family=Coming+Soon&family=Lora:ital,wght@0,400;0,700;1,400;1,700&family=Poppins:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">--}}
+
+
+{{--<link rel="stylesheet" href="/build/tailwind.css">--}}
+<link href="{{ asset('css/app.css') }}" rel="stylesheet">
