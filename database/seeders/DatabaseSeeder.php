e56819076cf0e0ba2e774340632f6487ed17ab62
Yan Siegers <email@yansiegers.nl>
2021-05-19 17:13:40 +0200

    Revert "Add physician and meeting factory"
    This reverts commit 6cb69867b210baec4f034cdd82ba60c7b512f68e.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-        // \App\Models\Physician::factory(10)->create();
-        \App\Models\Meeting::factory(10)->create();
+        \App\Models\User::factory(10)->create();
     }
 }
