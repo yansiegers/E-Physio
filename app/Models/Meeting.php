8493b9cdc5602c562436f3b28138b85fa58f125a
Yan Siegers <email@yansiegers.nl>
2021-05-19 17:13:28 +0200

    Revert "Add models and relations"
    This reverts commit 0b1c8ac08376b8d4b6309b9cba15e2ec9c858603.

--- app/Models/Meeting.php
+++ app/Models/Meeting.php
@@ -8,20 +8,4 @@ use Illuminate\Database\Eloquent\Model;
 class Meeting extends Model
 {
     use HasFactory;
-
-    /**
-     * Get the user that belongs to the meeting.
-     */
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
-
-    /**
-     * Get the physician that owns the meeting.
-     */
-    public function physician()
-    {
-        return $this->belongsTo(Physician::class);
-    }
 }
--- app/Models/Physician.php
+++ app/Models/Physician.php
@@ -8,12 +8,4 @@ use Illuminate\Database\Eloquent\Model;
 class Physician extends Model
 {
     use HasFactory;
-
-    /**
-     * Get the meetings for the physician.
-     */
-    public function meetings()
-    {
-        return $this->hasMany(Meeting::class);
-    }
 }
