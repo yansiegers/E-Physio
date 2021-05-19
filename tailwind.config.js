fd9032247e8ae182a2f3d465ff11b4bc49af8874
Yan Siegers <email@yansiegers.nl>
2021-05-19 15:11:48 +0200

    Add custom colors and form classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,40 @@
 module.exports = {
   purge: [
-    './storage/framework/views/*.php',
-    './resources/**/*.blade.php',
-    './resources/**/*.js',
+    "./storage/framework/views/*.php",
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
     // './resources/**/*.vue',
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "eph-blue": {
+          DEFAULT: "#0089d0",
+          light: "#f2f9fc",
+        },
+        "eph-purple": {
+          DEFAULT: "#a850ff",
+          light: "#f6edff",
+        },
+        "eph-black": {
+          DEFAULT: "#2c303d",
+          light: "#9fa3b6",
+        },
+        "eph-grey": {
+          DEFAULT: "#929db1",
+          light: "#f3f7fa",
+        },
+      },
+      // fontFamily: {
+      //   sans: ["Poppins", "sans-serif"],
+      //   serif: ["Lora", "serif"],
+      //   cursive: ["Coming Soon", "cursive"],
+      // },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
