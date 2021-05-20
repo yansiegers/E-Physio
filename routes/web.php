2917a4b13aaf76891526243cc10829c143fd7e10
Yan Siegers <email@yansiegers.nl>
2021-05-20 13:15:08 +0200

    Install Laravel Breeze

--- routes/web.php
+++ routes/web.php
@@ -13,42 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// ========= General pages ======================================================
-
 Route::get('/', function () {
-    return view('pages.home');
-})->name('home');
-
-Route::get('/over', function () {
-    return view('pages.about');
-})->name('about');
-
-Route::get('/contact', function () {
-    return view('pages.contact');
-})->name('contact');
-
-Route::get('/veel-gestelde-vragen', function () {
-    return view('pages.faq');
-})->name('faq');
-
-Route::get('/blog', function () {
-    return view('pages.blog');
-})->name('blog');
-
-// ========= Portal ======================================================
+    return view('welcome');
+});
 
 Route::get('/dashboard', function () {
-    return view('portal.dashboard');
-})->name('dashboard');
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-Route::get('/kalender', function () {
-    return view('portal.calendar');
-})->name('calendar');
-
-Route::get('/oefeningen', function () {
-    return view('portal.exercises');
-})->name('exercises');
-
-Route::get('/account', function () {
-    return view('portal.account');
-})->name('account');
+require __DIR__.'/auth.php';
