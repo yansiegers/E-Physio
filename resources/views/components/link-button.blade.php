ea0a2e8a9d13e722a10d532554dd9c447f2f42f9
Yan Siegers <email@yansiegers.nl>
2021-05-21 14:13:16 +0200

    Add link button

---
+++ resources/views/components/link-button.blade.php
@@ -0,0 +1,5 @@
+<a {{ $attributes->merge(
+    ['class' => 'btn font-bold text-center no-underline inline-block']
+    ) }}>
+    {{ $slot }}
+</a>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,4 +2,20 @@
 
 @section('content')
     <h1 class="text-center">E-Physio</h1>
+
+    <x-link-button href="/" class="btn-blue rounded-full">
+        {{ __('Blue') }}
+    </x-link-button>
+
+    <x-link-button href="/" class="btn-purple rounded-full">
+        {{ __('Purple') }}
+    </x-link-button>
+
+    <x-link-button href="/" class="btn-blue-light rounded-full">
+        {{ __('Blue Light') }}
+    </x-link-button>
+
+    <x-link-button href="/" class="btn-purple-light rounded-full">
+        {{ __('Purple Light') }}
+    </x-link-button>
 @stop
