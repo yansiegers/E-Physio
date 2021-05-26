0026b640ab93822a1e5ec869a134aeebad9a525c
Yan Siegers <email@yansiegers.nl>
2021-05-26 18:02:06 +0200

    Fix date format

--- resources/views/components/forms/input.blade.php
+++ resources/views/components/forms/input.blade.php
@@ -16,4 +16,4 @@
     }
 @endphp
 
-<input {{ $attributes->class([$classes, 'disabled' => $attributes->has('disabled')])->merge(['type' => $type]) }}>
+<input {{ $attributes->class([$classes, 'disabled' => $attributes->has('disabled')])->merge(['type' => $type, 'pattern' => "\d{4}-\d{2}-\d{2}"]) }}>
