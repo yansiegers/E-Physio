3b3274ac6e7a642d800feaabe108d5b075388826
Bas1997 <basoerlemans1@gmail.com>
2021-06-08 13:16:36 +0200

    50/50 grids homepage design

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,8 +1,155 @@
 <x-general-layout>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+
     <div class="container mx-auto py-5 prose mb-32">
         <span class="text-eph-purple font-bold">Online fysiotherapie</span>
         <h1 class="heading-h1">Waar en wanneer het jou uitkomt</h1>
         <p class="text-eph-grey">Behandeld worden door professionele fysiotherapeuten door heel Nederland.</p>
         <x-links.button class="btn-primary rounded-full mt-4" href="#">Hoe werkt het?</x-link-button>
     </div>
+
+
+    <!--Start section1-->
+    <section id="usp" class="relative py-20 bg-white text-black">
+        <div class="container mx-auto px-4">
+
+            <!--Left-->
+            <div class="items-center flex flex-wrap">
+                <div class="w-full md:w-5/12 ml-auto mr-auto px-4">
+                    <div class="md:pr-12">
+                        <!--<small class="text-eph-purple">Dummy sub</small>-->
+                        <h2 class="heading-h2">E-physio is geschikt voor:</h2>
+
+                    <!--Bullet list-->
+                        <ul class="list-none mt-6">
+                            <li class="py-2">
+                                <div class="flex flex-items">
+                                    <div>
+                                        <span class="inline-block text-eph-blue mr-3"><i class="bi bi-check"></i></span>
+                                    </div>
+                                    <div>
+                                        <p class="text-eph-grey">Overal beschikbaar</p>
+                                    </div>
+                                </div>
+                            </li>
+
+                            <li class="py-2">
+                                <div class="flex flex-items">
+                                    <div>
+                                        <span class="inline-block text-eph-blue mr-3"><i class="bi bi-check"></i></span>
+                                    </div>
+                                    <div>
+                                        <p class="text-eph-grey">Online contact</p>
+                                    </div>
+                                </div>
+                            </li>
+
+                            <li class="py-2">
+                                <div class="flex flex-items">
+                                    <div>
+                                        <span class="inline-block text-eph-blue mr-3"><i class="bi bi-check"></i></span>
+                                    </div>
+                                    <div>
+                                        <p class="text-eph-grey">Wanneer het jou uitkomt</p>
+                                    </div>
+                                </div>
+                            </li>
+
+                            <li class="py-2">
+                                <div class="flex flex-items">
+                                    <div>
+                                        <span class="inline-block text-eph-blue mr-3"><i class="bi bi-check"></i></span>
+                                    </div>
+                                    <div>
+                                        <p class="text-eph-grey">Persoonlijke begeleiding</p>
+                                    </div>
+                                </div>
+                            </li>
+
+                            <li class="py-2">
+                                <div class="flex flex-items">
+                                    <div>
+                                        <span class="inline-block text-eph-blue mr-3"><i class="bi bi-check"></i></span>
+                                    </div>
+                                    <div>
+                                        <p class="text-eph-grey">e-consult €29,95</p>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+            <!--Right-->
+                <div class="w-full md:w-5/12 ml-auto mr-auto px-4">
+                <img class="rounded" src="https://cdn.pixabay.com/photo/2016/06/04/01/27/business-woman-1434836_1280.jpg"/>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+
+    <!--Start section2-->
+    <section id="usp" class="relative py-20 bg-white text-black">
+        <div class="container mx-auto px-4">
+
+        <!--Left-->
+            <div class="items-center flex flex-wrap">
+                <div class="w-full md:w-5/12 ml-auto mr-auto px-4">
+                    <img class="rounded" src="https://images.unsplash.com/photo-1551288049-bebda4e38f71?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"/>
+                </div>
+
+
+        <!--Right-->
+                <div class="w-full md:w-5/12 ml-auto mr-auto px-4">
+                    <div class="md:pr-12">
+                    <!--<small class="text-eph-purple">Dummy sub</small>-->
+                    <h2 class="heading-h2">Kies jouw fysiotherapeut</h2>
+                    <p class="text-eph-grey mt-6">Het idee voor online fysiotherapie is ontstaan uit passie voor het vak en de eigenzinnigheid in ons werk...</p>
+                    <p class="mt-10 text-eph-purple"><a href="#">Lees verder<i class="bi bi-arrow-right-short"></i></a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+   <!--Start section3-->
+   <section id="usp" class="relative py-20 bg-white text-black">
+    <div class="container mx-auto px-4">
+
+        <!--Left-->
+        <div class="items-center flex flex-wrap">
+            <div class="w-full md:w-5/12 ml-auto mr-auto px-4">
+                <div class="md:pr-12">
+                    <!--<small class="text-eph-purple">Dummy sub</small>-->
+                    <h2 class="heading-h2">Altijd toegang <br>tot jouw behandelplan</h2>
+                    <p class="text-eph-grey mt-6">Wij willen jou de beste zorg geven en dus hoort daar de laatste wetenschap bij. We hebben een aantal studies voor je samengevat...</p>
+                    <p class="mt-10 text-eph-purple"><a href="#">Lees verder<i class="bi bi-arrow-right-short"></i></a></p>
+                </div>
+            </div>
+
+        <!--Right-->
+            <div class="w-full md:w-5/12 ml-auto mr-auto px-4">
+            <img class="rounded" src="https://cdn.pixabay.com/photo/2016/06/04/01/27/business-woman-1434836_1280.jpg"/>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 </x-general-layout>
